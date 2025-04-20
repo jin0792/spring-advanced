030615b08be22800d2e51f7ad18f2ed6283020ec
mixedsider <dmstjd0812@gmail.com>
2025-04-20 20:01:14 +0900

    Lv 2. N+1 문제

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,13 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    // N+1 문제의 가장 일반적인 해결 방법은 fetch join
+    // fetch join은 기본 설정을 lazy로 걸고 inner join 수행. left를 붙여 outer join을 수행
+
+    // EntityGraph는 Spring Data JPA에서 제공하는 것으로 연관된 엔티티들을 SQL 한번에 조회하는 방법
+    // EntityGraph는 기본 설정을 eager로 변환하고 left outer join으로 연관된 데이터를 끌어온다.
+    @EntityGraph(attributePaths = "user")
+
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
